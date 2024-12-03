497cffaced438f8e75b33d051911bee15e4fca6b
The-Infinitys <theinfinitysinfinity@gmail.com>
2024-12-03 09:46:06 +0000

    asdf

--- main.cpp
+++ main.cpp
@@ -11,10 +11,14 @@ int main(int, char**) {
     terminal::clear();
     terminal::set_cursor_pos(0, 0);
     for (int i = 0; i < 360; ++i) {
-        print(terminal::color_txt_hsv("Hello!",(double)i, 1, 1));
+        terminal::clear();
+        terminal::set_txt(terminal::color_txt_hsv("Hello!", (double)i, 1, 1),
+                          i % 5, 1);
+        terminal::renew();
         msleep((int)(1000 / 30));
     }
     sleep(1);
+    terminal::clear();
+    terminal::set_cursor_pos(0, 0);
     return 0;
 }
-
