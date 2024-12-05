449fbe78463ae8dcdb2707aa99ff6ab5d8283103
The-Infinitys <theinfinitysinfinity@gmail.com>
2024-12-05 13:05:58 +0900

    with '#' will be ignored, and an empty message aborts the commit.

---
+++ build/CMakeFiles/Infinity-task-manager.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Infinity-task-manager.dir/main.cpp.o"
+  "Infinity-task-manager"
+  "Infinity-task-manager.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Infinity-task-manager.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
