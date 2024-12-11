31fd90c2c35c519838ffddb581bc1274e386c90c
The-Infinitys <theinfinitysinfinity@gmail.com>
2024-12-11 03:11:23 +0000

    今後はtopからデータを丸写しする様にする

---
+++ build/CMakeFiles/Infinity-task-manager.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Infinity-task-manager.dir/main.cpp.o"
+  "CMakeFiles/Infinity-task-manager.dir/main.cpp.o.d"
+  "Infinity-task-manager"
+  "Infinity-task-manager.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Infinity-task-manager.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- library/app.hpp
+++ library/app.hpp
@@ -5,70 +5,26 @@
 namespace application {
 int title_color = 0;
 int term_lines = 0;
-void draw_cpu_usage() {
-    terminal::draw_line(2, "#00ff00");
-    std::vector<std::string> mpstat_data =
-        string_split(terminal::cmd("mpstat -u -P ALL"), '\n');
-    std::string cpu_name = mpstat_data[0];
-    int idle_data_position = mpstat_data[2].find("idle") - 1;
-    terminal::set_txt(
-        terminal::color_txt_rgb(
-            "CPU: " + cpu_name.substr(0, cpu_name.find("\t")), "#00ff00"),
-        2, 3);
-    std::vector<double> cpu_usage_info(mpstat_data.size() - 3, 0);
-    for (int i = 3; i < mpstat_data.size(); i++) {
-        double usage_var =
-            100.0 - std::stod(mpstat_data[i].substr(idle_data_position,
-                                                    mpstat_data[i].length()));
-        cpu_usage_info[i - 3] = usage_var;
-    }
-    for (int i = 0; i < cpu_usage_info.size(); i++) {
-        if (i == 0) {
-            terminal::set_txt(
-                "ALL-CPU: " + std::to_string(cpu_usage_info[i]) + "%", 2,
-                i + 4);
-        } else {
-            terminal::set_txt("CPU-" + std::to_string(i) + ": " +
-                                  std::to_string(cpu_usage_info[i]) + "%",
-                              2, i + 4);
-        }
-    }
-    term_lines += cpu_usage_info.size() + 4;
-}
-void draw_process_info() {
-    terminal::draw_line(term_lines, "#ffff00");
-    term_lines++;
-    std::vector<std::string> process_info_data =
-        string_split(terminal::cmd("top -b -n 1"), '\n');
+std::vector<std::string> get_machine_process_info() {
+    return string_split(terminal::cmd("top -b -n 1"), '\n');
     ;
-    int term_height = terminal::term_size()[1];
-    bool is_general_info = false;
-    int data_count = 0;
-    while (term_height > term_lines) {
-        if (process_info_data[data_count] == "") {
-            is_general_info = true;
-        }
-        if (is_general_info) {
-            terminal::set_txt(
-                terminal::color_txt_rgb(process_info_data[data_count], "#005500"), 2,
-                term_lines);
-        } else {
-            terminal::set_txt(process_info_data[data_count], 2, term_lines);
-        }
-        data_count++;
-        term_lines++;
-    }
 }
+
 void program() {
-    term_lines = 0;
+    term_lines = 1;
     title_color += 1;
     title_color %= 360;
     std::vector<int> term_info = terminal::term_size();
     terminal::set_txt(terminal::color_txt_hsv("The Infinity's task manager",
                                               title_color, 1, 1),
-                      2, 0);
-    draw_cpu_usage();
-    draw_process_info();
+                      2, term_lines);
+    term_lines++;
+    std::vector<std::string> process_info = get_machine_process_info();
+    for (int i = 0; i < process_info.size(); i++) {
+        terminal::set_txt(process_info[i], 2, term_lines);
+        ++term_lines;
+    }
+
     terminal::draw_col(0, color_hsv(title_color, 1, 1));
 }
 }  // namespace application
