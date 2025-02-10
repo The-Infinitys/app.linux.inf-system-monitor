415c69f99db6854e074e015fe2739121c6a4dd6e
The Infinity's <137460810+The-Infinitys@users.noreply.github.com>
2025-02-10 07:15:29 +0000

    asdfg

--- __main__.py
+++ __main__.py
@@ -1,2 +1,34 @@
 from lib import manage
-import textual
+from lib import textual
+import os, sys
+from textual.app import App, ComposeResult
+from textual.widgets import Static, Header, Footer, Button
+
+class InfinitySystemMonitor(App):
+    CSS = """
+    Screen {
+      align: center top;
+    }
+    #cpu-usage {
+      color: red;
+    }
+    #mem-usage{
+      color: yellow;
+    }
+    Static {
+    border: heavy white;
+    }
+    """
+    def quit_sysmonitor(self) -> None:
+        sys.exit(0)
+
+    def compose(self) -> ComposeResult:
+        # "yield Widgetのクラス"で、ターミナルにWidgetを表示する
+        yield Header("The Infinity's System Monitor")
+        yield Static("CPU USAGE", id="cpu-usage")
+        yield Static("MEMORY USAGE", id="mem-usage")
+        yield Footer()
+
+if __name__ == "__main__":  
+    app = InfinitySystemMonitor()
+    app.run()
